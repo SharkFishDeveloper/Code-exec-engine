442243ab844479f59097f8c7e2e8044b4b8351ed
OKIDOKI\shahz <shahzebakhtar66@gmail.com>
2025-03-17 20:57:36 +0530

    anfdafs

--- cpp.sh
+++ cpp.sh
@@ -15,33 +15,31 @@ if [ "$FILE_EXTENSION" == "c" ]; then
     clang -o output_program "$CODE_FILE" 2> compile_error.txt
 elif [ "$FILE_EXTENSION" == "cpp" ]; then
     clang++ -o output_program "$CODE_FILE" 2> compile_error.txt
+else
+    echo "Invalid language specified. Use 'c' or 'cpp'."
+    exit 1
 fi
+
 # Check if there were any compilation errors
 if [ $? -ne 0 ]; then
     echo -e "Compilation failed!\n"
     cat compile_error.txt
     exit 1
 fi
-# Run the compiled program and capture runtime errors
-./output_program > output.txt 2> runtime_error.txt
 
-# Check if there were any runtime errors
-if [ $? -ne 0 ]; then
-    echo -e "Runtime error!\n"
-    cat runtime_error.txt
-    exit 1
-
-fi
-# start
-if [ -f "/usr/src/app/cpp-engine/app/input.txt" ]; then
+# Check if input file exists
+INPUT_FILE="/usr/src/app/cpp-engine/app/input.txt"
+if [ -f "$INPUT_FILE" ]; then
     # Run the compiled program with input redirection
-    ./output_program < /usr/src/app/cpp-engine/app/input.txt > output.txt 2> runtime_error.txt
+    ./output_program < "$INPUT_FILE" > output.txt 2> runtime_error.txt
 else
     # Run the compiled program without input redirection
     ./output_program > output.txt 2> runtime_error.txt
 fi
-# end
+
+# Check if there were any runtime errors
 if [ $? -ne 0 ]; then
+    echo -e "Runtime error!\n"
     cat runtime_error.txt
     exit 1
 fi
